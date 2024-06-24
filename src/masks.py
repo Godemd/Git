572672462cdde6f8641f8a6a950594d3c5cc0658
Godemd <dandigrom1@gmail.com>
2024-06-24 19:31:33 +0300

    homework_13_2

--- src/masks.py
+++ src/masks.py
@@ -1,29 +1,45 @@
-def mask_card_number(card_number: str) -> str:
-    """
-    ### Маскирует номер карты, показывая только первые 4 цифры, последние 4 цифры и скрывая остальные.
+from src.utils import setup_logger
 
-    Аргументы:
-        card_number (str): Номер карты, который нужно замаскировать.
-
-    Возвращается:
-        str: Замаскированный номер карты.
-    """
-    front_part = card_number[:4]
-    middle_part = card_number[4:6]
-    end_part = card_number[-4:]
-
-    masked_number = f"{front_part} {middle_part}** **** {end_part}"
-    return masked_number
+logger = setup_logger("masks")
 
 
 def mask_account_number(account_number: str) -> str:
     """
-    ## Маскирует номер счета, показывая только последние 4 цифры.
+    ### Маскирует номер карты/счёта
+    #### Для номера карты, показывая только первые 4 цифры, последние 4 цифры и скрывая остальные.
+    #### Для номера счёта, показывая только последние 4 цифры.
+
 
     Аргументы:
-        account_number (str): Номер счета для маскировки.
+        account_number (str): Номер карты/счёта, который нужно замаскировать.
 
-    Возвращает:
-        str: Замаскированный номер счета.
+    Возвращается:
+        str: Замаскированный номер карты/счёта.
     """
-    return f"**{account_number[-4:]}"
+    logger.info(f"Исходный номер карты: {account_number}")
+
+    if not account_number.isdigit():
+        logger.error("Номер карты должен содержать только цифры")
+        raise ValueError("Номер должен содержать только цифры")
+
+    if isinstance(account_number, int):
+        logger.error("Номер карты должен быть строкой")
+        raise ValueError("Номер должен быть строкой")
+
+    if len(account_number) == 16:
+
+        front_part = account_number[:4]
+        middle_part = account_number[4:6]
+        end_part = account_number[-4:]
+
+        masked_number = f"{front_part} {middle_part}** **** {end_part}"
+        logger.info(f"Замаскированный номер карты: {masked_number}")
+        return masked_number
+
+    if len(account_number) == 20:
+        masked_number = f"**{account_number[-4:]}"
+        logger.info(f"Замаскированный номер счёта: {masked_number}")
+        return masked_number
+
+    logger.error("Неверный формат номера")
+    raise ValueError("Неверный формат номера")
--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,74 @@
 import json
+import logging
 import os
 
+import pandas as pd
 
-def load_transactions(file_path: str) -> list:
+
+def setup_logger(name: str) -> logging.Logger:
     """
-    Возвращает список словарей из JSON-строки, содержащей данные о финансовых транзакциях.
-
+    ## Настройка логгера
     Аргументы:
-    file_path (str): Путь к файлу JSON.
-
+        `name (str)`: Имя логгера
     Возвращает:
-    list: Список словарей с данными о финансовых транзакциях.
+        `logging.Logger`: Объект логгера
+    """
+    file_path = os.path.join("logs", f"{name}.log")
 
-    Если файл не существует, возвращается пустой список.
-    Если файл содержит некорректные данные или не может быть прочитан, также возвращается пустой список.
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s %(levelname)-7s %(name)s:%(lineno)d -> %(message)s")
+
+    logger_file_handler = logging.FileHandler(file_path, encoding="utf-8", mode="w")
+    logger_file_handler.setFormatter(formatter)
+
+    logger.addHandler(logger_file_handler)
+
+    return logger
+
+
+logger = setup_logger("utils")
+
+
+def read_data_transactions(file_path: str) -> list[dict]:
+    """
+    ## Возвращает список словарей из JSON/CSV/XLSX
+    Аргументы:
+        `file_path (str)`: Путь к JSON/CSV/XLSX-файлу
+    Возвращает:
+        `list`: список словарей
     """
     if not os.path.exists(file_path):
+        logger.warning(f"Файл {file_path} не найден")
         return []
 
-    with open(file_path, "r", encoding="utf-8") as file:
-        try:
-            data = json.load(file)
-            if isinstance(data, list):
-                return data
-            else:
+    if file_path.endswith(".json"):
+        with open(file_path, "r", encoding="utf-8") as file:
+            try:
+                data = json.load(file)
+                if isinstance(data, list):
+                    logger.info(f"Файл {file_path} успешно загружен")
+                    return data
+
+                else:
+                    logger.error(f"Файл {file_path} содержит некорректные данные")
+                    return []
+
+            except json.JSONDecodeError:
+                logger.error(f"Файл {file_path} содержит некорректные данные")
                 return []
-        except json.JSONDecodeError:
-            return []
 
+    elif file_path.endswith(".csv"):
+        df = pd.read_csv(file_path, delimiter=";")
+        logger.info(f"Файл {file_path} успешно загружен")
+        return df.to_dict("records")
 
-# # Пример использования
-# file_path = "data\operations.json"
-# transactions = load_transactions(file_path)
-# print("Загруженные транзакции:", transactions)
+    elif file_path.endswith(".xlsx"):
+        df = pd.read_excel(file_path)
+        logger.info(f"Файл {file_path} успешно загружен")
+        return df.to_dict("records")
+
+    else:
+        logger.error(f"Неподдерживаемый формат файла {file_path}")
+        return []
